d60fbd6e42eec68fd16efc1de3601e46cf0d6583
png <jasona99@outlook.com>
2020-11-04 13:58:37 -0500

    Updated with structure changes and example

--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
+#include <camerabytes.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 void app_main() {
-    
+	int i = 0;
+	while (1) {
+		printf("[%d] Hello world!\n", i);
+		i++;
+		vTaskDelay(5000 / portTICK_PERIOD_MS);
+	}
 }
